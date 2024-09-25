a9b900b35f9a1291d6949fd65fcebda44a383521
mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-09-25 14:49:07 +0800

    0925

---
+++ utilk_android/src/main/java/com/mozhimen/kotlin/elemk/commons/IHasId.kt
@@ -0,0 +1,12 @@
+package com.mozhimen.kotlin.elemk.commons
+
+/**
+ * @ClassName IHasId
+ * @Description TODO
+ * @Author mozhimen
+ * @Date 2024/9/25
+ * @Version 1.0
+ */
+interface IHasId {
+    val id: String
+}
