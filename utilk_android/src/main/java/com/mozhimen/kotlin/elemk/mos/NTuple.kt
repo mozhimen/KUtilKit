b58078e1f506e3073e031ff08a76724f35093cef
Kolin Zhao & Mozhimen <mozhimen@163.com>
2024-10-16 10:26:03 +0800

    1016

--- utilk_android/src/main/java/com/mozhimen/kotlin/elemk/mos/NTuple.kt
+++ utilk_android/src/main/java/com/mozhimen/kotlin/elemk/mos/NTuple.kt
@@ -13,5 +13,27 @@ data class NTuple2<T1, T2>(var t1: T1, var t2: T2) {
     }
 }
 data class NTuple3<T1, T2, T3>(var t1: T1, var t2: T2, var t3: T3)
-data class NTuple4<T1, T2, T3, T4>(var t1: T1, var t2: T2, var t3: T3, var t4: T4)
+data class NTuple4<T1, T2, T3, T4>(var t1: T1, var t2: T2, var t3: T3, var t4: T4){
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as NTuple4<*, *, *, *>
+
+        if (t1 != other.t1) return false
+        if (t2 != other.t2) return false
+        if (t3 != other.t3) return false
+        if (t4 != other.t4) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = t1?.hashCode() ?: 0
+        result = 31 * result + (t2?.hashCode() ?: 0)
+        result = 31 * result + (t3?.hashCode() ?: 0)
+        result = 31 * result + (t4?.hashCode() ?: 0)
+        return result
+    }
+}
 data class NTuple5<T1, T2, T3, T4, T5>(var t1: T1, var t2: T2, var t3: T3, var t4: T4, var t5: T5)
