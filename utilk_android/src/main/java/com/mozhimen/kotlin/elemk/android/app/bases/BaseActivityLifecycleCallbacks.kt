ab43f9e7c8c68a338471b818b0bf8d843a909b2b
mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-11-04 11:20:48 +0800

    1104

--- utilk_android/src/main/java/com/mozhimen/kotlin/elemk/android/app/bases/BaseActivityLifecycleCallbacks.kt
+++ utilk_android/src/main/java/com/mozhimen/kotlin/elemk/android/app/bases/BaseActivityLifecycleCallbacks.kt
@@ -3,6 +3,7 @@ package com.mozhimen.kotlin.elemk.android.app.bases
 import android.app.Activity
 import android.app.Application
 import android.os.Bundle
+import android.util.Log
 import com.mozhimen.kotlin.utilk.android.util.UtilKLogWrapper
 import com.mozhimen.kotlin.utilk.commons.IUtilK
 
@@ -15,86 +16,86 @@ import com.mozhimen.kotlin.utilk.commons.IUtilK
  */
 open class BaseActivityLifecycleCallbacks : Application.ActivityLifecycleCallbacks, IUtilK {
     override fun onActivityPreCreated(activity: Activity, savedInstanceState: Bundle?) {
-        UtilKLogWrapper.v(TAG,"onActivityPreCreated___________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name} savedInstanceState $savedInstanceState" )
+        Log.v(TAG,"onActivityPreCreated___________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name} savedInstanceState $savedInstanceState" )
     }
 
     override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-        UtilKLogWrapper.d(TAG,"onActivityCreated______________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name} savedInstanceState $savedInstanceState" )
+        Log.d(TAG,"onActivityCreated______________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name} savedInstanceState $savedInstanceState" )
     }
 
     override fun onActivityPostCreated(activity: Activity, savedInstanceState: Bundle?) {
-        UtilKLogWrapper.v(TAG,"onActivityPostCreated__________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name} savedInstanceState $savedInstanceState" )
+        Log.v(TAG,"onActivityPostCreated__________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name} savedInstanceState $savedInstanceState" )
     }
 
     override fun onActivityPreStarted(activity: Activity) {
-        UtilKLogWrapper.v(TAG,"onActivityPreStarted___________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.v(TAG,"onActivityPreStarted___________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 
     override fun onActivityStarted(activity: Activity) {
-        UtilKLogWrapper.d(TAG,"onActivityStarted______________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.d(TAG,"onActivityStarted______________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 
     override fun onActivityPostStarted(activity: Activity) {
-        UtilKLogWrapper.v(TAG,"onActivityPostStarted__________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.v(TAG,"onActivityPostStarted__________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 
     override fun onActivityPreResumed(activity: Activity) {
-        UtilKLogWrapper.v(TAG,"onActivityPreResumed___________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.v(TAG,"onActivityPreResumed___________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 
     override fun onActivityResumed(activity: Activity) {
-        UtilKLogWrapper.d(TAG,"onActivityResumed______________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.d(TAG,"onActivityResumed______________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 
     override fun onActivityPostResumed(activity: Activity) {
-        UtilKLogWrapper.v(TAG,"onActivityPostResumed__________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.v(TAG,"onActivityPostResumed__________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 
     override fun onActivityPrePaused(activity: Activity) {
-        UtilKLogWrapper.v(TAG,"onActivityPrePaused____________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.v(TAG,"onActivityPrePaused____________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 
     override fun onActivityPaused(activity: Activity) {
-        UtilKLogWrapper.d(TAG,"onActivityPaused_______________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.d(TAG,"onActivityPaused_______________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 
     override fun onActivityPostPaused(activity: Activity) {
-        UtilKLogWrapper.v(TAG,"onActivityPostPaused___________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.v(TAG,"onActivityPostPaused___________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 
     override fun onActivityPreStopped(activity: Activity) {
-        UtilKLogWrapper.v(TAG,"onActivityPreStopped___________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.v(TAG,"onActivityPreStopped___________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 
     override fun onActivityStopped(activity: Activity) {
-        UtilKLogWrapper.d(TAG,"onActivityStopped______________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.d(TAG,"onActivityStopped______________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 
     override fun onActivityPostStopped(activity: Activity) {
-        UtilKLogWrapper.v(TAG,"onActivityPostStopped__________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.v(TAG,"onActivityPostStopped__________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 
     override fun onActivityPreSaveInstanceState(activity: Activity, outState: Bundle) {
-        UtilKLogWrapper.v(TAG,"onActivityPreSaveInstanceState_ activity ${activity.javaClass.simpleName} ${activity.javaClass.name} outState $outState")
+        Log.v(TAG,"onActivityPreSaveInstanceState_ activity ${activity.javaClass.simpleName} ${activity.javaClass.name} outState $outState")
     }
 
     override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-        UtilKLogWrapper.d(TAG,"onActivitySaveInstanceState____ activity ${activity.javaClass.simpleName} ${activity.javaClass.name} outState $outState")
+        Log.d(TAG,"onActivitySaveInstanceState____ activity ${activity.javaClass.simpleName} ${activity.javaClass.name} outState $outState")
     }
 
     override fun onActivityPostSaveInstanceState(activity: Activity, outState: Bundle) {
-        UtilKLogWrapper.v(TAG,"onActivityPaused activity ${activity.javaClass.simpleName} ${activity.javaClass.name} outState $outState")
+        Log.v(TAG,"onActivityPaused activity ${activity.javaClass.simpleName} ${activity.javaClass.name} outState $outState")
     }
 
     override fun onActivityPreDestroyed(activity: Activity) {
-        UtilKLogWrapper.v(TAG,"onActivityPreDestroyed_________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.v(TAG,"onActivityPreDestroyed_________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 
     override fun onActivityDestroyed(activity: Activity) {
-        UtilKLogWrapper.d(TAG,"onActivityDestroyed____________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.d(TAG,"onActivityDestroyed____________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 
     override fun onActivityPostDestroyed(activity: Activity) {
-        UtilKLogWrapper.v(TAG,"onActivityPostDestroyed________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
+        Log.v(TAG,"onActivityPostDestroyed________ activity ${activity.javaClass.simpleName} ${activity.javaClass.name}")
     }
 }
