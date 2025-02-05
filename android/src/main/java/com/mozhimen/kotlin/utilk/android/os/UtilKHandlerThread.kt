9bc4d97455a67802f85a55918d64fba8c38ce5d6
Kolin Zhao & Mozhimen <mozhimen@163.com>
2025-02-05 09:54:42 +0800

    250205

---
+++ android/src/main/java/com/mozhimen/kotlin/utilk/android/os/UtilKHandlerThread.kt
@@ -0,0 +1,11 @@
+package com.mozhimen.kotlin.utilk.android.os
+
+/**
+ * @ClassName UtilKHandlerThread
+ * @Description TODO
+ * @Author Mozhimen / Kolin Zhao
+ * @Date 2025/2/5 0:24
+ * @Version 1.0
+ */
+object UtilKHandlerThread {
+}
